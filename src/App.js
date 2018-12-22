6f10d6f9a4b6f7a2aee925695a95e36b2fb3afe1
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2018-12-22 12:47:59 -0200

    Add createGlobalStyle
    createGlobalStyle is a helper funcion from styled-components that handle global styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
 
+import GlobalStyle from './GlobalStyle';
+
 import ListOfNotes from './containers/ListOfNotes';
 
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ListOfNotes />
+      <GlobalStyle />
     </PersistGate>
   </Provider>
 );
---
+++ src/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+   padding: 0;
+   margin: 0; 
+  }
+  @import url('https://fonts.googleapis.com/css?family=Lato:400,700|Slabo+27px');
+`;
+
+export default GlobalStyle;
