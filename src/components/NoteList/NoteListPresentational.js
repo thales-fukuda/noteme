7ea93c9e29cbf71f2061a872069a2c333b85cb56
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2018-12-23 22:19:40 -0200

    Fix bubbling
    There was a bubbling on the nav notes and the delete note button.

--- src/components/NoteList/NoteListPresentational.js
+++ src/components/NoteList/NoteListPresentational.js
@@ -8,22 +8,32 @@ import icMenu from '../../assets/icons/menu.svg';
 import icMinus from '../../assets/icons/minus.svg';
 import icPlus from '../../assets/icons/plus.svg';
 
-class NoteList extends Component {
+class NoteListPresentational extends Component {
   state = { menuOpen: false };
 
   toggleNav = () => {
     this.setState(prevState => ({
       menuOpen: !prevState.menuOpen,
     }));
-  }
+  };
+
+  handleActiveNote = (e, id) => {
+    e.stopPropagation();
+    const { activeNote } = this.props;
+    activeNote(id);
+  };
+
+  handleRemoveNote = (e, id) => {
+    e.stopPropagation();
+    const { removeNote } = this.props;
+    removeNote(id);
+  };
 
   render() {
     const { menuOpen } = this.state;
     const {
       notes,
       addNote,
-      removeNote,
-      activeNote,
     } = this.props;
     return (
       <Menu bgColor='#F9CA24'>
@@ -31,9 +41,9 @@ class NoteList extends Component {
         <Menu.Button icon={icPlus} onClick={addNote} />
         <Menu.Nav className={menuOpen ? 'active' : ''} bgColor='#E5E5E5'>
           {notes.map(el => (
-            <Menu.Note key={el.id} onClick={() => activeNote(el.id)}>
+            <Menu.Note key={el.id} onClick={e => this.handleActiveNote(e, el.id)}>
               <Menu.Note.Title>{el.noteTitle}</Menu.Note.Title>
-              <Menu.Button icon={icMinus} onClick={() => removeNote(el.id)} />
+              <Menu.Button icon={icMinus} onClick={e => this.handleRemoveNote(e, el.id)} />
             </Menu.Note>
           ))}
         </Menu.Nav>
@@ -42,11 +52,11 @@ class NoteList extends Component {
   }
 }
 
-NoteList.propTypes = {
+NoteListPresentational.propTypes = {
   notes: PropTypes.instanceOf(Array).isRequired,
   addNote: PropTypes.func.isRequired,
   removeNote: PropTypes.func.isRequired,
   activeNote: PropTypes.func.isRequired,
 };
 
-export default NoteList;
+export default NoteListPresentational;
