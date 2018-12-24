e2f43e4e77907dace3a7d6c1a2a645aebac6b4ac
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2018-12-24 19:07:12 -0200

    Updates

---
+++ precache-manifest.e41a2db30bffa0468c7fe50c080ee1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "41a86ddcc7108dfd2bc64198c070b41a",
+    "url": "/noteme/static/media/plus.41a86ddc.svg"
+  },
+  {
+    "revision": "bdb8b89e879171eafddf0e0be638e8a2",
+    "url": "/noteme/static/media/minus.bdb8b89e.svg"
+  },
+  {
+    "revision": "b18eadddea1de90c841fdfa58641c1c0",
+    "url": "/noteme/static/media/menu.b18eaddd.svg"
+  },
+  {
+    "revision": "abca3781a712484ef84a",
+    "url": "/noteme/static/js/runtime~main.abca3781.js"
+  },
+  {
+    "revision": "ee7a406396f1526cd6ca",
+    "url": "/noteme/static/js/main.ee7a4063.chunk.js"
+  },
+  {
+    "revision": "133617e00c6aa52281a4",
+    "url": "/noteme/static/js/1.133617e0.chunk.js"
+  },
+  {
+    "revision": "dec928617887c7f4c1a2125b74de2cec",
+    "url": "/noteme/index.html"
+  }
+];
