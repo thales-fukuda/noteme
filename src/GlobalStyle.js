968b5ac62a5b9b7fe04f56930ba2f04d99ef5504
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2018-12-24 19:17:29 -0200

    Fix textarea style on desktop

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,11 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Lato:400,700|Slabo+27px');
   * {
    padding: 0;
    margin: 0; 
   }
-  @import url('https://fonts.googleapis.com/css?family=Lato:400,700|Slabo+27px');
+  textarea {
+    border: none;
+    overflow: auto;
+    outline: none;
+
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+
+    resize: none;
+  }
 `;
 
 export default GlobalStyle;
