f2e9586b55c0f874d3cf5c1da3435b4ff88b1330
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2018-12-24 19:18:17 -0200

    Fix Navbar scroll bug

--- src/components/blocks/Menu/Nav.js
+++ src/components/blocks/Menu/Nav.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-  height: calc(100% - 70px);
+  height: calc(100vh - 70px);
   width: 75%;
   position: fixed;
   top: 70px;
@@ -9,6 +9,7 @@ const Nav = styled.nav`
   background-color: ${props => props.bgColor};
   transform: translate3d(-100%, 0, 0);
   transition: ease .3s transform;
+  overflow: scroll;
   &.active{
     transform: translate3d(0, 0, 0);
   }
