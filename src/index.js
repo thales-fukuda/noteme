339f5b6843f2e04d14c3c64d2efd27321942571b
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2019-02-17 10:38:08 -0300

    Add todo

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+/**
+ * TODO:
+ * React Hooks
+ * Import things on Index
+ * Alias(?)
+ */
