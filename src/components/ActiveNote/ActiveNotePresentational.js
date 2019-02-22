77598f53be77829239000cb50b2e12550ae38272
thales-fukuda <thalesc@alunos.utfpr.edu.br>
2019-02-22 00:27:50 -0300

    Add react hooks
    https://reactjs.org/docs/hooks-intro.html
    
    React Hooks is a new feature in react that enables the usage of class component features on functional components.

--- src/components/ActiveNote/ActiveNotePresentational.js
+++ src/components/ActiveNote/ActiveNotePresentational.js
@@ -1,64 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import NoteWrapper from '../blocks/NoteWrapper';
 
-class ActiveNotePresentational extends Component {
-  constructor(props) {
-    super(props);
-    this.textTitle = React.createRef();
-    this.textBody = React.createRef();
-  }
+const ActiveNotePresentational = (props) => {
+  const textTitle = useRef();
+  const textBody = useRef();
+  const {
+    notes, activeNote, changeNoteTitle, changeNoteBody,
+  } = props;
 
-  componentDidMount = () => {
-    const { notes } = this.props;
-    if (notes.length > 0) {
-      this.changeTextAreaSize(this.textTitle.current);
-      this.changeTextAreaSize(this.textBody.current);
-    }
-  };
-
-  changeTextAreaSize = (el) => {
+  const changeTextAreaSize = (el) => {
     const element = el;
     element.style.height = `${element.scrollHeight}px`;
   };
 
-  handleChangeTitle = (e, id) => {
+  const handleChangeTitle = (e, id) => {
     e.stopPropagation();
-    const { changeNoteTitle } = this.props;
     changeNoteTitle(id, e.target.value);
-    this.changeTextAreaSize(e.target);
+    changeTextAreaSize(e.target);
   };
 
-  handleChangeBody = (e, id) => {
+  const handleChangeBody = (e, id) => {
     e.stopPropagation();
-    const { changeNoteBody } = this.props;
     changeNoteBody(id, e.target.value);
-    this.changeTextAreaSize(e.target);
+    changeTextAreaSize(e.target);
   };
 
-  render() {
-    const { notes, activeNote } = this.props;
-    return (
-      <NoteWrapper>
-        {notes.length > 0
-        && <>
+  useEffect(() => {
+    if (notes.length > 0) {
+      changeTextAreaSize(textTitle.current);
+      changeTextAreaSize(textBody.current);
+    }
+  });
+
+  return (
+    <NoteWrapper>
+      {notes.length > 0
+      && (
+        <>
           <NoteWrapper.TitleText
-            onChange={e => this.handleChangeTitle(e, notes[activeNote].id)}
-            ref={this.textTitle}
+            onChange={e => handleChangeTitle(e, notes[activeNote].id)}
+            ref={textTitle}
             value={notes[activeNote].noteTitle}
           />
           <NoteWrapper.Text
-            onChange={e => this.handleChangeBody(e, notes[activeNote].id)}
-            ref={this.textBody}
+            onChange={e => handleChangeBody(e, notes[activeNote].id)}
+            ref={textBody}
             value={notes[activeNote].noteBody}
           />
         </>
-        }
-      </NoteWrapper>
-    );
-  }
-}
+      )
+      }
+    </NoteWrapper>
+  );
+};
 
 ActiveNotePresentational.propTypes = {
   notes: PropTypes.instanceOf(Array).isRequired,
--- src/components/NoteList/NoteListPresentational.js
+++ src/components/NoteList/NoteListPresentational.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Menu from '../blocks/Menu';
@@ -8,67 +8,63 @@ import icMenu from '../../assets/icons/menu.svg';
 import icMinus from '../../assets/icons/minus.svg';
 import icPlus from '../../assets/icons/plus.svg';
 
-class NoteListPresentational extends Component {
-  state = { menuOpen: false };
+const NoteListPresentational = (props) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const {
+    notes,
+    addNote,
+    activeNote,
+    removeNote,
+    updateActiveNote,
+  } = props;
 
-  toggleNav = () => {
-    this.setState(prevState => ({
-      menuOpen: !prevState.menuOpen,
-    }));
+  const toggleNav = () => {
+    setMenuOpen(!menuOpen);
   };
 
-  handleActiveNote = (e, id) => {
+  const handleActiveNote = (e, id) => {
     e.stopPropagation();
-    const { updateActiveNote } = this.props;
     updateActiveNote(id);
     if (!window.matchMedia(' (min-width: 726px) ').matches) {
-      this.toggleNav();
+      toggleNav();
     }
   };
 
-  handleRemoveNote = (e, id) => {
+  const handleRemoveNote = (e, id) => {
     e.stopPropagation();
-    const { removeNote } = this.props;
     removeNote(id);
   };
 
-
-  render() {
-    const { menuOpen } = this.state;
-    const {
-      notes,
-      addNote,
-      activeNote,
-    } = this.props;
-    return (
-      <Menu bgColor='#F9CA24'>
-        {
-          window.matchMedia(' (min-width: 726px) ').matches
-            ? <Menu.Button icon={icPlus} onClick={addNote} />
-            : <>
-              <Menu.Button icon={icMenu} onClick={this.toggleNav} />
+  return (
+    <Menu bgColor='#F9CA24'>
+      {
+        window.matchMedia(' (min-width: 726px) ').matches
+          ? <Menu.Button icon={icPlus} onClick={addNote} />
+          : (
+            <>
+              <Menu.Button icon={icMenu} onClick={toggleNav} />
               <Menu.Button icon={icPlus} onClick={addNote} />
-              </>
+            </>
+          )
+      }
+      <Menu.Nav className={menuOpen ? 'active' : ''} bgColor='#F2F2F2'>
+        {notes.length > 0
+        && notes.map((el, index) => (
+          <Menu.Note
+            key={el.id}
+            onClick={e => handleActiveNote(e, el.id)}
+            active={activeNote === index ? '1' : '.5'}
+          >
+            <Menu.Note.Title>{el.noteTitle}</Menu.Note.Title>
+            <Menu.Button icon={icMinus} onClick={e => handleRemoveNote(e, el.id)} />
+          </Menu.Note>
+        ))
         }
-        <Menu.Nav className={menuOpen ? 'active' : ''} bgColor='#F2F2F2'>
-          {notes.length > 0
-          && notes.map((el, index) => (
-            <Menu.Note
-              key={el.id}
-              onClick={e => this.handleActiveNote(e, el.id)}
-              active={activeNote === index ? '1' : '.5'}
-            >
-              <Menu.Note.Title>{el.noteTitle}</Menu.Note.Title>
-              <Menu.Button icon={icMinus} onClick={e => this.handleRemoveNote(e, el.id)} />
-            </Menu.Note>
-          ))
-          }
-        </Menu.Nav>
-        { menuOpen && <Menu.CloseMenu onClick={this.toggleNav} /> }
-      </Menu>
-    );
-  }
-}
+      </Menu.Nav>
+      { menuOpen && <Menu.CloseMenu onClick={toggleNav} /> }
+    </Menu>
+  );
+};
 
 NoteListPresentational.propTypes = {
   notes: PropTypes.instanceOf(Array).isRequired,
